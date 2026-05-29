6e06d7b547b5242c536d7fd97e8106259df91d44
Jamison <hunterjamison6@gmail.com>
2026-05-28 23:13:33 -0600

    Added byte amounts to files

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,26 @@ use std::fs;
 fn main() {
     let paths = fs::read_dir("./").unwrap();
     let mut file_names: Vec<String> = Vec::new();
+    let mut file_sizes: Vec<u64> = Vec::new();
 
     for path in paths {
-        file_names.push(path.unwrap().path().display().to_string());
+        let entry = path.unwrap();
+        let path_buf = entry.path();
+
+        file_names.push(path_buf.display().to_string());
+
+        let metadata = fs::metadata(&path_buf).unwrap();
+        let file_size = metadata.len();
+
+        file_sizes.push(file_size);
     }
 
+    let mut index = 0;
     for name in file_names {
-        let file = fs::File::create(&name).unwrap();
-        let file_size = file.metadata().unwrap().len();
+        // let file_size = file.metadata().unwrap().len();
 
         let name = &name[2..];
-        println!("{} {}", name, file_size);
+        println!("{} {} bytes", name, file_sizes[index]);
+        index += 1;
     }
 }
