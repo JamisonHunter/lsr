af8345815548035c0618469fa17048dc28935da9
Jamison <hunterjamison6@gmail.com>
2026-05-28 22:25:53 -0600

    Renamed variables more accurately

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::fs;
 
 fn main() {
     let paths = fs::read_dir("./").unwrap();
-    let mut files: Vec<String> = Vec::new();
+    let mut file_names: Vec<String> = Vec::new();
 
     for path in paths {
-        files.push(path.unwrap().path().display().to_string());
+        file_names.push(path.unwrap().path().display().to_string());
     }
 
-    for file in files {
-        let name = &file[2..];
+    for name in file_names {
+        let name = &name[2..];
         println!("{}", name);
     }
 }
