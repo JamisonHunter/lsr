1a682e546daf52942834f3fb2bce90cd2d3e70de
Jamison <hunterjamison6@gmail.com>
2026-05-29 18:52:20 -0600

    Added recursive file sizing

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,45 @@ struct Document {
     is_dir: bool,
 }
 
+fn format_bytes(bytes: u64) -> String {
+    if bytes >= 1000000000 {
+        let gigabytes: f64 = bytes as f64 / 1000000000 as f64;
+        return format!("{:.1} Gb", gigabytes);
+    }
+    if bytes >= 1000000 {
+        let megabytes: f64 = bytes as f64 / 1000000 as f64;
+        return format!("{:.1} Mb", megabytes);
+    }
+    if bytes >= 1000 {
+        let kilobytes: f64 = bytes as f64 / 1000 as f64;
+        return format!("{:.1} Kb", kilobytes);
+    }
+    return format!("{} bytes", bytes);
+}
+
+fn get_dir_size(file_path: String) -> u64 {
+    let path_str = format!("./{}", file_path);
+    let paths = fs::read_dir(path_str).unwrap();
+    let mut total_size: u64 = 0;
+
+    for path in paths {
+        let entry = path.unwrap();
+        let path_buf = entry.path();
+
+        let metadata = fs::metadata(&path_buf).unwrap();
+
+        let name: String = path_buf.display().to_string();
+
+        if metadata.is_dir() {
+            total_size += get_dir_size(name[2..].to_string());
+        } else {
+            total_size += metadata.len();
+        }
+
+    }    
+    return total_size;
+}
+
 fn main() {
     let paths = fs::read_dir("./").unwrap();
     let mut documents: Vec<Document> = Vec::new();
@@ -30,9 +69,10 @@ fn main() {
 
     for document in documents {
         if document.is_dir {
-            println!("{}", format!("{} {} bytes", document.name, document.size).blue());
+            let file_size = get_dir_size(document.name.clone());
+            println!("{}", format!("{} {}", document.name, format_bytes(file_size)).blue());
         } else {
-            println!("{} {} bytes", document.name, document.size);
+            println!("{} {}", document.name, format_bytes(document.size));
         }
     }
 }
